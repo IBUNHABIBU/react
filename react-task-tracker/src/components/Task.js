54752f3978dbc667868be3a0043eb07aefb154b5
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-24 22:27:59 +0300

    adding task

---
+++ react-task-tracker/src/components/Task.js
@@ -0,0 +1,37 @@
+import React from 'react'
+const tasks = [
+  {
+  id:1,
+  text: 'Doctors Appointment',
+  day: "Feb 5th at 2:30pm",
+  remainder: true,
+  },
+  {
+    id:2,
+    text: 'Playing golf',
+    day: "Feb 21 at 10:00",
+    remainder: true,
+  },
+  {
+      id:1,
+      text: 'Eating Ugali',
+      day: "March 5th at 2:30pm",
+      remainder: true,
+  },
+  {
+        id:1,
+        text: 'Doctors Appointment',
+        day: "Feb 5th at 2:30pm",
+        remainder: true,
+ },
+
+]
+function Task() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Task
