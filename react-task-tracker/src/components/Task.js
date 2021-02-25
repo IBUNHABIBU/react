e3555b7eabf037866844395b202ac8e81cbf36df
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-25 14:19:55 +0300

    adding state

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,32 +1,33 @@
-import React from 'react'
-const tasks = [
-  {
-  id:1,
-  text: 'Doctors Appointment',
-  day: "Feb 5th at 2:30pm",
-  remainder: true,
-  },
-  {
-    id:2,
-    text: 'Playing golf',
-    day: "Feb 21 at 10:00",
+import React from 'react';
+import { useState } from 'react';
+const Task = () => {
+  const [tasks, setTasks] = useState([
+    {
+    id:1,
+    text: 'Doctors Appointment',
+    day: "Feb 5th at 2:30pm",
     remainder: true,
-  },
-  {
-    id:3,
-    text: 'Eating Ugali',
-    day: "March 5th at 2:30pm",
-    remainder: true,
-  },
-  {
-    id:4,
-    text: 'Food shopping',
-    day: "Feb 16th at 21:30pm",
-    remainder: true,
- },
+    },
+    {
+      id:2,
+      text: 'Playing golf',
+      day: "Feb 21 at 10:00",
+      remainder: true,
+    },
+    {
+      id:3,
+      text: 'Eating Ugali',
+      day: "March 5th at 2:30pm",
+      remainder: true,
+    },
+    {
+      id:4,
+      text: 'Food shopping',
+      day: "Feb 16th at 21:30pm",
+      remainder: true,
+   },
+  ])
 
-]
-function Task() {
   return (
     <>
       { 
