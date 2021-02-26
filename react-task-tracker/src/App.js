e61e313f78948813ace798b7cb8439f2274a5e9e
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-26 21:23:17 +0300

    modifying the component

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header'
-import Task from './components/Task'
+import Task from './components/Tasks'
 import { useState } from 'react';
 function App() {
   const [tasks, setTasks] = useState([
