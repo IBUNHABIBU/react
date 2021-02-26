cc49af634f6be1694f8358913358f48e92b718fe
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-26 23:56:18 +0300

    delete task id

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -4,7 +4,7 @@ function Task({task, onDelete }) {
   return (
     <div className="task">
       <h3> {task.text} <FaTimes style = { { color:'red', cursor:'pointer'} } 
-      onClick = { onDelete } /> </h3>
+      onClick = { () => { onDelete(task.id) }} /> </h3>
       <p> {task.day} </p>
     </div>
   )
