f97be22022614c0ddfd33604196167935e4160bd
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-26 23:53:27 +0300

    ondelete working

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -3,7 +3,8 @@ import { FaTimes } from 'react-icons/fa';
 function Task({task, onDelete }) {
   return (
     <div className="task">
-      <h3> {task.text} <FaTimes style = { { color:'red', cursor:'pointer'} } /> </h3>
+      <h3> {task.text} <FaTimes style = { { color:'red', cursor:'pointer'} } 
+      onClick = { onDelete } /> </h3>
       <p> {task.day} </p>
     </div>
   )
