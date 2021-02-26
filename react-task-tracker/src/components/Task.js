8b5edfe2e7168c638c6752180314c54b420b05a8
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-26 23:50:56 +0300

    deleting task' g

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa';
-function Task({task}) {
+function Task({task, onDelete }) {
   return (
     <div className="task">
       <h3> {task.text} <FaTimes style = { { color:'red', cursor:'pointer'} } /> </h3>
