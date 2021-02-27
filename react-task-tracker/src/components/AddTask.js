c286a72ee491980521242aa7c62c845c3765b0ff
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-27 22:56:18 +0300

    adding form

--- react-task-tracker/src/components/AddTask.js
+++ react-task-tracker/src/components/AddTask.js
@@ -3,7 +3,10 @@ import React from 'react'
 function AddTask() {
   return (
     <form className='add-form'>
-      
+      <div className="form-control">
+        <label htmlFor="">Task </label>
+        <input type="text" name="" id="" placeholder= 'Add Task'/>
+      </div>
     </form>
   )
 }
