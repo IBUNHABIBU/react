8bf00ab1e979c52eced4319763a5ece84850bd62
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-28 23:15:25 +0300

    set task

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -38,7 +38,9 @@ function App() {
     setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder} : task))
   }
   const addTask = (task) => {
-    console.log(task)
+   const id = Math.floor(Math.random()*1000) + 1;
+   const newTask = { id, ...task};
+   setTasks(...tasks, newTask)
   }
   return (
     <div className="container">
