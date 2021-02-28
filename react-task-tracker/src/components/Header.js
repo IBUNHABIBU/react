488e3270fd4eab519df2d321dd1c09202fde0237
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-28 23:31:36 +0300

    commenting

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
-const Header = ({title}) => {
+const Header = ({title, onAdd }) => {
   
   return (
     <header className ="header">
       <h6>{title}</h6>
-      <Button color='green' text='submit' />
-      <Button   color='blue' text='Add' />
+      {/* <Button color='green' text='submit' /> */}
+      <Button   color='green' text='Add' />
       <Button color='red' text='cancel' />
     </header>
   )
