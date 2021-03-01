8531daf906ce8fed89923c1f2a9188ff1934c2f8
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-01 15:30:41 +0300

    show add method

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
-const Header = ({title, onAdd }) => {
+const Header = ({title, onAdd, showAdd }) => {
  
   return (
     <header className ="header">
       <h6>{title}</h6>
       
-      <Button   color='green' text='Add' onClick = { onAdd }/>
+      <Button 
+        color = { showAdd ? 'red':'green'}
+        text= { showAdd ? 'Close': 'Add' }
+         onClick = { onAdd }/>
       <Button color='red' text='cancel' />
     </header>
   )
