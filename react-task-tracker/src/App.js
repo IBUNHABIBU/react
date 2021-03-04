b2835ea2073d4b63ed8be5ea78f6cf65a5283d4b
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-04 22:05:53 +0300

    adding link

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import  AddTask  from "./components/AddTask";
 import { useState, useEffect } from 'react';
