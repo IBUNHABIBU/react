635c0ff65437ffea99fd8dfdd0d3d2c5d97c3ca5
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-05 23:35:19 +0300

    user locationl

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
-import { userLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 const Header = ({title, onAdd, showAdd }) => {
- 
+  const location = useLocation();
   return (
     <header className ="header">
       <h6>{title}</h6>
       
-      <Button 
+      { location.pathname === '/' && (<Button 
         color = { showAdd ? 'red':'green'}
         text= { showAdd ? 'Close': 'Add' }
-         onClick = { onAdd }/>
+         onClick = { onAdd }/>)}
       {/* <Button color='red' text='cancel' /> */}
     </header>
   )
