bc9f9a4def967365c4beee0c5c5f85b9c2939cd9
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-27 22:03:17 +0300

    styling the page

--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -3,8 +3,10 @@ import React, { Component } from "react";
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick = {function() {
+            console.log("clicked")
+        }}>
+          {this.props.value}
         </button>
       );
     }
