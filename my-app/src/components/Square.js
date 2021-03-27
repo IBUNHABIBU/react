3125c2e1fd99bb9d0090379f16f3e61c42b338fb
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-27 22:11:34 +0300

    styling the page

--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -9,10 +9,12 @@ class Square extends React.Component {
     }
     render() {
       return (
-        <button className="square" onClick = {function() {
-            console.log("clicked")
+        <button className="square" onClick = { () =>{
+            this.setState({
+                value: "X"
+            })
         }}>
-          {this.props.value}
+          {this.state.value}
         </button>
       );
     }
