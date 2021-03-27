471caf3dec865dba3ee7d02c3325e0a6fd07ebaf
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-27 22:06:50 +0300

    styling the page

--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 class Square extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
     render() {
       return (
         <button className="square" onClick = {function() {
