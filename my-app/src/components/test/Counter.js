dee7dbce5a50fbe176e106b32936f9fb969d2ab6
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-28 15:49:44 +0300

    adding new component

--- my-app/src/components/test/Counter.js
+++ my-app/src/components/test/Counter.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react'
 
+export class Counter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
     }
 }
+
+export default Counter
