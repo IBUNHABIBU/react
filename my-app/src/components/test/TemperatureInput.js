395f271c59779f55e985711228932d92ce5fccd4
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-10 19:08:02 +0300

    adding temperature input

--- my-app/src/components/test/TemperatureInput.js
+++ my-app/src/components/test/TemperatureInput.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+  };
+  
 export default class TemperatureInput extends Component {
     constructor(props) {
         super(props)
