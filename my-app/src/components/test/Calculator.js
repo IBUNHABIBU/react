79acb4de38d43a09969ce49da0a62ac40b96ae30
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-10 18:36:13 +0300

    adding temp calculator

---
+++ my-app/src/components/test/Calculator.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Calculator extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {
+             temperature: ""
+        }
+    }
+    handleChange = (e) => 
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
