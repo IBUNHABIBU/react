6c1af70c1299a80e0afb6fc0c702a9e89075539e
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-10 18:27:33 +0300

    adding verdict file

--- my-app/src/components/test/BoilingVerdict.js
+++ my-app/src/components/test/BoilingVerdict.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const BoilingVerdict = (props) => {
    if( props.celcious >= 100) {
-       return <p></p>
+       return <p>The water would boil</p>
    }
+   return <p>The water will not boil</p>
 }
 
 export default BoilingVerdict
