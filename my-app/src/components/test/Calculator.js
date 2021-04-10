c586e98d55dee152d55a3abdb407cc77997fc1a0
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-10 23:37:16 +0300

    lifting up state

--- my-app/src/components/test/Calculator.js
+++ my-app/src/components/test/Calculator.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 
 import BoilingVerdict from "./BoilingVerdict";
 import TemperatureInput from "./TemperatureInput";
+
+
+  function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+  
+  function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+  
+  function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  }
+  
 export default class Calculator extends Component {
     constructor(props) {
         super(props)
