2b1190a4af4d82ab64f3a0b95abb4baed71cf951
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-10 19:12:27 +0300

    adding temp input

--- my-app/src/components/test/TemperatureInput.js
+++ my-app/src/components/test/TemperatureInput.js
@@ -8,9 +8,11 @@ const scaleNames = {
 export default class TemperatureInput extends Component {
     constructor(props) {
         super(props)
-     this.handleChange = this.handleChange.bind(this);
-     this.state = {temperature: ''};
-     
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
     }
     
     render() {
