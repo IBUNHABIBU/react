78d929427918c2dfece3e1f19185244fe115445e
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-12 19:28:56 +0300

    next player

--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick ={function(){
+          
+      } }>
         { this.props.value }
       </button>
     );
