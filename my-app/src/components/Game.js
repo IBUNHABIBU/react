ed32dd0798bd96faa0bd58ab0b04d756fe3fbcee
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-12 19:11:09 +0300

    next player

--- my-app/src/components/Game.js
+++ my-app/src/components/Game.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-        
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
         </div>
-      );
-    }
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
   }
+}
 
   export default Game;
