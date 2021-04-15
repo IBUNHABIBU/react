071ef15ce5cccd6c1a5647bfbdb0a5bc5d1eb268
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-15 18:53:30 +0300

    lifting up states

--- my-app/src/components/test/BoilingVerdict.js
+++ my-app/src/components/test/BoilingVerdict.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
 const BoilingVerdict = (props) => {
-   if( props.celcious >= 100) {
-       return <p>The water would boil</p>
-   }
-   return <p>The water will not boil</p>
+  
 }
 
 export default BoilingVerdict
