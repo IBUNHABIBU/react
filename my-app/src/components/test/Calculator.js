7aace380d225add4319c0ba0966900fc4d0067cd
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-15 19:20:26 +0300

    lifting up states

--- my-app/src/components/test/Calculator.js
+++ my-app/src/components/test/Calculator.js
@@ -4,5 +4,16 @@ import BoilingVerdict from "./BoilingVerdict";
 import TemperatureInput from "./TemperatureInput";
 
 export default class Calculator extends Component {
-  
+  constructor(props) {
+      super(props)
+      this.handleChange = this.handleChange.bind(this)
+      this.state = {
+           temperature: ''
+      }
+  }
+  handleChange(e) {
+      this.setState({
+          temperature: e.target.value 
+      })
+  }
 }
