db946c5f2c850446e0c7a5a52f1f940e7a913571
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-15 18:56:07 +0300

    lifting up states

--- my-app/src/components/test/Calculator.js
+++ my-app/src/components/test/Calculator.js
@@ -3,4 +3,6 @@ import React, { Component } from 'react'
 import BoilingVerdict from "./BoilingVerdict";
 import TemperatureInput from "./TemperatureInput";
 
+export default class Calculator extends Component {
+  
 }
