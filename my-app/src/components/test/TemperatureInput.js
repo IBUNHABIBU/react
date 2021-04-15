2d950b50e1a8903593b95d84dd1330f1c7272180
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-15 20:30:15 +0300

    lifting up states

--- my-app/src/components/test/TemperatureInput.js
+++ my-app/src/components/test/TemperatureInput.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react'
-
+const scaleNames = {
+    c: 'Celcious',
+    f: 'Fahreinheit'
+}
 export default class TemperatureInput extends Component {
+   constructor(props) {
+       super(props)
+   
+       this.state = {
+            temperature: ''
+       }
+   }
    
 }
