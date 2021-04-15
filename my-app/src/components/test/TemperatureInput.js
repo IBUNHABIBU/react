8579a675238a399d31359d40424eab0fac51180b
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-15 20:49:02 +0300

    lifting up states

--- my-app/src/components/test/TemperatureInput.js
+++ my-app/src/components/test/TemperatureInput.js
@@ -6,7 +6,7 @@ const scaleNames = {
 export default class TemperatureInput extends Component {
    constructor(props) {
        super(props)
-   
+        this.handleChange = this.handleChange.bind(this)
        this.state = {
             temperature: ''
        }
