e075a34521422d86410b1ee3c357b796a38e28fe
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-17 14:46:34 +0300

    changing square to function component

--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -1,19 +1,4 @@
-// import React, { Component } from "react";
 
-// class Square extends React.Component {
-  
-//   render() {
-//     return (
-//       <button className="square" onClick ={()=>
-//           this.props.onClick()
-//        }>
-//         { this.props.value }
-//       </button>
-//     );
-//   }
-// }
-
-//   export default Square;
 import React from 'react'
 
 const Square = (props) => {
