2b589f3289be495a022640d624bc68a84c488a80
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-18 23:13:24 +0300

    adding isOperator logic

--- flexbox/src/components/Buttons.js
+++ flexbox/src/components/Buttons.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Buttons.css';
 
+const isOperator = val => {
+    return !isNaN(val) || val ==='.' || val === '='
+}
 const Buttons = props => {
     return (
         <div>
