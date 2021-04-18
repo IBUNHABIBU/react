bb9668abfedd9149bdaa5781884083dee16e8b32
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-18 23:37:03 +0300

    styling the buttons

--- flexbox/src/App.js
+++ flexbox/src/App.js
@@ -9,16 +9,28 @@ export default class App extends Component {
       <div className="app">
           <div className="calc-wrapper">
             <div className="row">
-                <Buttons>1</Buttons>
-                <Buttons>2</Buttons>
-                <Buttons>3</Buttons>
-                <Buttons>+</Buttons>
+                <Buttons>7</Buttons>
+                <Buttons>8</Buttons>
+                <Buttons>9</Buttons>
+                <Buttons>/</Buttons>
+            </div>
+            <div className="row">
+                <Buttons>4</Buttons>
+                <Buttons>5</Buttons>
+                <Buttons>6</Buttons>
+                <Buttons>*</Buttons>
             </div>
             <div className="row">
                 <Buttons>1</Buttons>
                 <Buttons>2</Buttons>
                 <Buttons>3</Buttons>
                 <Buttons>+</Buttons>
+            </div>
+            <div className="row">
+                <Buttons>.</Buttons>
+                <Buttons>0</Buttons>
+                <Buttons>=</Buttons>
+                <Buttons>-</Buttons>
             </div>
           </div>
       </div>
