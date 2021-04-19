2d81000ba6dffccf0e39d3c84b76b797f56bba60
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-19 20:36:35 +0300

    adding home page

--- iphone/src/components/App/index.jsx
+++ iphone/src/components/App/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
     return (
-        <div>
-            
+        <div className="app">
+            Hello world
         </div>
     )
 }
