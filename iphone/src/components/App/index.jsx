731778cfb37e32fb4bcf19ec4d893f55e90cd1ea
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-20 23:20:23 +0300

    adding type to buttons

--- iphone/src/components/App/index.jsx
+++ iphone/src/components/App/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Button from '../Button';
 const App = () => {
+    const [value, setstate] = useState("385")
     return (
         <div className="app">
             <div className="top">4:43</div>
-            <div className="display">0</div>
+            <div className="display"> { value } </div>
             <div className="buttons">
                 <Button children = "AC" type ="function" />
                 <Button children = "+/-" type ="function" />
