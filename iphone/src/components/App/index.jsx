b94d60f1187938047eaf37be9549c8ba42bb58be
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-20 20:01:56 +0300

    adding buttons

--- iphone/src/components/App/index.jsx
+++ iphone/src/components/App/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
+import Button from '../Button';
 const App = () => {
     return (
         <div className="app">
             <div className="top">4:43</div>
             <div className="display">0</div>
-            <div className="buttons">AC</div>
+            <div className="buttons">
+                <Button />
+            </div>
             <div className="bottom">-</div>
         </div>
     )
