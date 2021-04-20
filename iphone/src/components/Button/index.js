b93aa5c104e33e35dbb938795b05983f9c7e7cce
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-20 23:09:23 +0300

    adding type to buttons

--- iphone/src/components/Button/index.js
+++ iphone/src/components/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './buttons.css'
 const Button = ( { children, type } )=> {
     return (
-        <div className= {`button  ${ children === "0" ? "zero" : null }` }>
+        <div className= {`button  ${ children === "0" ? "zero" : null } ${ type || ""}` }>
             { children }
         </div>
     )
