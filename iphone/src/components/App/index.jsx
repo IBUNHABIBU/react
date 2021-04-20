fb52532c0b99ae68f481a0ce1298363a2f232a1c
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-20 21:54:15 +0300

    adding buttons

--- iphone/src/components/App/index.jsx
+++ iphone/src/components/App/index.jsx
@@ -8,6 +8,18 @@ const App = () => {
             <div className="display">0</div>
             <div className="buttons">
                 <Button children = "AC" />
+                <Button children = "+/-" />
+                <Button children = "%" />
+                <Button children = "/" />
+                <Button children = "7" />
+                <Button children = "8" />
+                <Button children = "9" />
+                <Button children = "*" />
+                
+                <Button children = "4" />
+                <Button children = "5" />
+                <Button children = "6" />
+                <Button children = "-" />
             </div>
             <div className="bottom">-</div>
         </div>
