c97f4642ebf65f6c2f6d7731da6106c3635ab2b6
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-20 23:24:47 +0300

    adding type to buttons

--- iphone/src/components/App/index.jsx
+++ iphone/src/components/App/index.jsx
@@ -2,35 +2,38 @@ import React, { useState } from 'react'
 import './App.css'
 import Button from '../Button';
 const App = () => {
-    const [value, setstate] = useState("385")
+    const [value, setstate] = useState("0")
+    const handleButtonClick = () => {
+
+    }
     return (
         <div className="app">
             <div className="top">4:43</div>
             <div className="display"> { value } </div>
             <div className="buttons">
-                <Button children = "AC" type ="function" />
-                <Button children = "+/-" type ="function" />
-                <Button children = "%" type ="function" />
-                <Button children = "/" type ="operator" />
-                <Button children = "7" />
-                <Button children = "8" />
-                <Button children = "9" />
-                <Button children = "*" type ="operator" />
+                <Button onButtonClick = { handleButtonClick } children = "AC" type ="function" />
+                <Button onButtonClick = { handleButtonClick } children = "+/-" type ="function" />
+                <Button onButtonClick = { handleButtonClick } children = "%" type ="function" />
+                <Button onButtonClick = { handleButtonClick } children = "/" type ="operator" />
+                <Button onButtonClick = { handleButtonClick } children = "7" />
+                <Button onButtonClick = { handleButtonClick } children = "8" />
+                <Button onButtonClick = { handleButtonClick } children = "9" />
+                <Button onButtonClick = { handleButtonClick } children = "*" type ="operator" />
                 
-                <Button children = "4" />
-                <Button children = "5" />
-                <Button children = "6" />
-                <Button children = "-" type = "operator" />
+                <Button onButtonClick = { handleButtonClick } children = "4" />
+                <Button onButtonClick = { handleButtonClick } children = "5" />
+                <Button onButtonClick = { handleButtonClick } children = "6" />
+                <Button onButtonClick = { handleButtonClick } children = "-" type = "operator" />
 
-                <Button children = "1" />
-                <Button children = "2" />
-                <Button children = "3" />
-                <Button children = "+" type ="operator" />
+                <Button onButtonClick = { handleButtonClick } children = "1" />
+                <Button onButtonClick = { handleButtonClick } children = "2" />
+                <Button onButtonClick = { handleButtonClick } children = "3" />
+                <Button onButtonClick = { handleButtonClick } children = "+" type ="operator" />
                 
                 
-                <Button children = "0" />
-                <Button children = "." />
-                <Button children = "=" type ="operator" />
+                <Button onButtonClick = { handleButtonClick } children = "0" />
+                <Button onButtonClick = { handleButtonClick } children = "." />
+                <Button onButtonClick = { handleButtonClick } children = "=" type ="operator" />
             
             </div>
             <div className="bottom">-</div>
