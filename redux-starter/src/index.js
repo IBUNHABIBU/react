0cd2b3bf638b8f0d1166a6881f671443d607e172
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-25 23:07:32 +0300

    function programming

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,14 +1,1 @@
-function sayHello() {
-    return function(){
-        return "Hello world"
-    }
-}
-
-function greet(fnMessage) {
-    console.log(fnMessage())
-}
-
-greet(sayHello)
-
-let fn = sayHello();
-let msg = fn()
+const trim = str => str.trim()
