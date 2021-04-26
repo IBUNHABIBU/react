eb17a9657b7585d0f6c4e76be998b961f943d9a8
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-26 21:41:58 +0300

    removing functional code

---
+++ redux-starter/src/index.js
@@ -0,0 +1,11 @@
+// const trim = str => str.trim()
+// const wrapDiv = str => `<div>${str}</div>`
+// const input = "   Javascript    "
+
+// const result = wrapDiv(trim(input))
+
+// const transform = pipe(wrapDiv,trim)
+
+const obj = {}
+obj.book = "dre"
+console.log(obj)
