60407d220a3f36ec89788e26bcaaa8da833593e4
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-26 21:40:56 +0300

    removing functional code

--- redux-starter/src/index.js
+++
@@ -1,7 +0,0 @@
-const trim = str => str.trim()
-const wrapDiv = str => `<div>${str}</div>`
-const input = "   Javascript    "
-
-const result = wrapDiv(trim(input))
-
-const transform = pipe(wrapDiv,trim)
