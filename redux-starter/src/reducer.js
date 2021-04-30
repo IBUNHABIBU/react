36e6412b0e1f8565acff593951f68f938f4ed7c6
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-30 18:10:17 +0300

    crating the reducer

--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -1,6 +1,6 @@
 // creating a reducer
 let lastId = 0
-function reducer(state, action) {
+export default function reducer(state = [], action) {
     if(action.type === 'bugAdded') {
         return [
             ...state, 
