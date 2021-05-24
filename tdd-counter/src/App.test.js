9b5012f3678ffb639402b318241923496af5c5bf
IBUNHABIBU <ibunhabib@gmail.com>
2021-05-24 17:03:09 +0300

    adding enzimes

--- tdd-counter/src/App.test.js
+++ tdd-counter/src/App.test.js
@@ -1,10 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Counter testing', () => {
-  test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+  test('renders the title of counter', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('h1').text()).toContain('this is counter app');
   });
 });
