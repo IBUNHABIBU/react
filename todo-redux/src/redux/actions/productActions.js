5a28e887eeb2ea5850c73223d041bdce3c23fd28
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-05 19:09:15 +0300

    creating a new file

---
+++ todo-redux/src/redux/actions/productActions.js
@@ -0,0 +1,15 @@
+import { actionTypes } from '../constants/actionTypes';
+
+export const setProducts = (products) => {
+    return {
+        type: actionTypes.SET_PRODUCT,
+        payload: products
+    }
+}
+
+export const selectProduct = (product) => {
+    return {
+        type: actionTypes.SELECT_PRODUCT,
+        payload: product
+    }
+}
