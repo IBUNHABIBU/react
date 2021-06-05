3bf8c529d685186c5bddb7e6730b0c5fdf3aa278
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-05 14:52:00 +0300

    adding another file

--- todo-redux/src/index.js
+++ todo-redux/src/index.js
@@ -1,33 +1,21 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import TodoApp from './TodoApp';
-// import { store } from './redux/store';
-// import { Provider } from 'react-redux';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoApp from './TodoApp';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <TodoApp />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <TodoApp />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
-function add(a) {
-  return function (b) {
-    return a + b ;
-  }
-}
-
-add(2);
-add(3)
-let result = add(4)(1);
-console.log(add(4));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
