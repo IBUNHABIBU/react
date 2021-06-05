c50737e767fc9b38d47d522d1c49ac75481a6cd9
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-05 18:54:58 +0300

    rearanging the files

--- todo-redux/src/index.js
+++ todo-redux/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 
 import { createStore } from 'redux';
-import allReducers from './reducers';
+import allReducers from './redux/reducers';
 
 const myStore = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 ReactDOM.render(
