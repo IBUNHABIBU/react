6af2236046d07374f8b5c933a748ff7d6b19ca08
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-06 18:53:17 +0300

    adding fakestore api

--- todo-redux/src/containers/ProductListing.js
+++ todo-redux/src/containers/ProductListing.js
@@ -7,8 +7,14 @@ const ProductListing = () => {
     const products = useSelector(state => state)
     
     const fetchProduct = async () => {
-        const response = await axios.get('https://fakestoreapi.com/products') 
+        const response = await axios.get('https://fakestoreapi.com/products').catch((err) => {
+            console.log("Error", err)
+        })
+        console.log(response.data[0].description)
     }
+    useEffect(() => {
+        fetchProduct();
+    }, [])
     return (
         <div className="ui grid container">
             <ProductComponent />
