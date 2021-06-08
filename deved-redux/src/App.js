f53e46b8a5458dde5635fb1ffed338704c993a05
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-08 15:43:27 +0300

    writing the reducer functions

--- deved-redux/src/App.js
+++ deved-redux/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
   return (
     <div className="App">
-      <h1>Welcom to redux</h1>
+      <h1>redux : { counter }</h1>
+        <button >+</button>
+        <button >-</button>
+      { isLogged? <h3>Valuable info I shouldn't see</h3> : '' }
     </div>
   );
 }
--- deved-redux/src/index.js
+++ deved-redux/src/index.js
@@ -4,12 +4,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 console.log(store);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store} >
+       <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
