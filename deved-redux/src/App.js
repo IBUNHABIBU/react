a1b620e61b99712e1179cc010a4b1509aafb00d3
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-08 16:04:09 +0300

    finished product

--- deved-redux/src/App.js
+++ deved-redux/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, login } from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter)
   const isLogged = useSelector(state => state.isLogged)
+ const dispatch = useDispatch()
   return (
     <div className="App">
+      <button onClick = { () =>
+        dispatch(login())
+      }>Click me to login</button>
       <h1>redux : { counter }</h1>
-        <button >+</button>
-        <button >-</button>
+
+        <button onClick = { () => dispatch(increment())}>+</button>
+        <button onClick = { () => dispatch(decrement())}>-</button>
       { isLogged? <h3>Valuable info I shouldn't see</h3> : '' }
     </div>
   );
