a8c999ccf02d09eb917b298e3789e89bc38d0e12
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-15 18:15:15 +0300

    adding components

--- fakestore-redux/src/App.js
+++ fakestore-redux/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './containers/Header';
+import { BrowserRouter as Router, Router, Route } from 'react-router-dom';
+import ProductListing from './containers/ProductListing';
 
 function App() {
   return (
     <div className="App">
-      <header/>
+      <Router>
+        <Header />
+        <Route path="/" exact component = {ProductListing} />
+      </Router>
+      
     </div>
   );
 }
