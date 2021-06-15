2c32dbad4c11678e77ebbe6dc94a3187dd60b180
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-15 18:00:34 +0300

    adding components

--- fakestore-redux/src/App.js
+++ fakestore-redux/src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Header from './containers/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header/>
     </div>
   );
 }
--- fakestore-redux/src/redux/reducers/productReducer.js
+++ fakestore-redux/src/redux/reducers/productReducer.js
@@ -1,3 +1,5 @@
+import { actionTypes } from "../constants/actionTypes";
+
 const initialState = {
     products: [{
         id: 1,
@@ -5,6 +7,12 @@ const initialState = {
         category: "Operation"
     }]
 }
-export const productReducer = (state, action) =>{
+export const productReducer = (state = initialState, {type, payload}) =>{
+    switch(type) {
+        case actionTypes.SET_PRODUCTS:
+            return state
+        default: 
+         return state
+    }
 
 };
