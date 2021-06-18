9f047d177d8d1624810ddf243b51db8ecfc58f0c
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-18 16:49:36 +0300

    connect mapstateto prop and mapdispatch to prop

--- codvolution-redux/src/App.js
+++ codvolution-redux/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CakeContainer from './container/CakeContainer';
 import { Provider } from 'react-redux';
 import './App.css';
+import store from './redux/store';
 
 function App() {
   return (
--- codvolution-redux/src/container/CakeContainer.js
+++ codvolution-redux/src/container/CakeContainer.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux'
 
-function CakeContainer() {
+function CakeContainer(props) {
     return (
         <div>
-            <h1>Number of cakes</h1>
-            <button>Buy cakes</button>
+            <h1>Number of cakes: {props.numberOfCakes}</h1>
+            <button onClick= { props.buyCake }>Buy cakes</button>
         </div>
     )
 }
 
-export default CakeContainer
+const mapStateToProps = state => {
+    return {
+        numberOfCakes: state.numberOfCakes,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
