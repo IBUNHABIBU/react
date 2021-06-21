384d08d9ed6a296d776738871a8aa65458f2a112
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-21 17:01:40 +0300

    subscribing to the store

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,5 +1,7 @@
 import store from './store';
 import { bugAdded } from './actions';
+
+store.subscribe(() => console.log("Store changed",store.getState()))
 store.dispatch(bugAdded("Bug 1"))
 
  store.dispatch({
