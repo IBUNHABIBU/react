2eca036bf16a03956cca6d0a6a70ccb646d34e2a
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-21 17:47:25 +0300

    bug resolved action creator

--- redux-starter/src/actions.js
+++ redux-starter/src/actions.js
@@ -5,7 +5,16 @@ export function bugAdded(description) {
     return {
         type: actions.BUG_ADDED,
         payload: {
-        description: "Bug1 string"
+        description
     }
     }
 }
+
+export const bugResolved = (id) => (
+    {
+        type: actions.BUG_RESOLVED,
+        payload: {
+            id
+        }
+    }
+)
