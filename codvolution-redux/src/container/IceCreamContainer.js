fccaa73c3a5283c945dc70bf904e8059514bb354
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-22 18:50:22 +0300

    adding icecream component

--- codvolution-redux/src/container/IceCreamContainer.js
+++ codvolution-redux/src/container/IceCreamContainer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import {buyIceCream} from '../redux/iceCream/IceCreamActions';
+import {buyIceCream} from '../redux/iceCream/iceCreamActions';
 
 function IceCreamContainer() {
     const numberOfIceCream = useSelector(state => state.numberOfIceCream)
     const dispatch = useDispatch()
     return (
         <div className="hooks-cake">
-            <h1>Use selector hook</h1>
+            <h1>Ice cream shop</h1>
             <h3>Number of Cake: { numberOfIceCream } </h3>
             <button onClick = {() => dispatch(buyIceCream())}> buyIceCream</button>
         </div>
