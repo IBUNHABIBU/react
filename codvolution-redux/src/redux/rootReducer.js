3aecc1ad6871dec01403048a0cc6818b57f50ad3
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-22 12:37:17 +0300

    combine reducers

--- codvolution-redux/src/redux/rootReducer.js
+++ codvolution-redux/src/redux/rootReducer.js
@@ -1,1 +1,10 @@
 import { combineReducers } from "redux";
+import iceCreamReducer from "./iceCream/iceCreamReducer";
+import cakeReducer from "./cake/cakeReducer";
+
+const rootReducer = combineReducers({
+    cake: iceCreamReducer,
+    iceCream: cakeReducer
+})
+
+export default rootReducer;
