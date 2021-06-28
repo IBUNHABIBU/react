c547360b9624924a009c3d7a82d3c7916f168919
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-28 19:23:47 +0300

    import button from material ui

--- material-ui-react/src/App.js
+++ material-ui-react/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button>
+        Hello
+      </Button>
     </div>
   );
 }
