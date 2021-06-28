d9f9a602fef066165e87b49e924af91e7ef8f492
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-28 19:29:48 +0300

    add click event

--- material-ui-react/src/App.js
+++ material-ui-react/src/App.js
@@ -4,7 +4,7 @@ import { Button } from '@material-ui/core';
 function App() {
   return (
     <div className="App">
-      <Button variant='contained' color="primary">
+      <Button variant='contained' color="primary" onClick = { () => alert('hello')}>
         Hello
       </Button>
     </div>
