3c1ed431bdede939589f5f60c7a8e9a2c1ff295a
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-03 20:40:32 +0300

    Import cards

--- material-ui-react/src/App.js
+++ material-ui-react/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
+import { Typography, AppBar, Card,
+   CardActions, CardContent, 
+   CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
 const App = () => {
   return (
     <div>
