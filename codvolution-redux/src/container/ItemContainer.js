70139836930643f0aa3a33399f4327baebb07d80
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-06 23:24:20 +0300

    Map state to props

--- codvolution-redux/src/container/ItemContainer.js
+++ codvolution-redux/src/container/ItemContainer.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const ItemContainer = () => {
+const ItemContainer = (props) => {
     return (
         <div>
-            
+            <h2>Item - { props.item } </h2>
         </div>
     )
 }
 
 const mapStateToProps = (state, ownProps) => {
-    const itemState = ownProps.cake ? state.cake.numOfCakes : state.iceCream.numOfIceCream
+    const itemState = ownProps.cake ? state.cake.numOfCakes : state.iceCream.numOfIceCreams;
+    return {
+        item: itemState,
+    }
 }
 
 export default ItemContainer
