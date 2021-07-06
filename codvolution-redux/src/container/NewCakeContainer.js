96ea360d1eaf2141d7fe7a6de3d14310da56df8b
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-06 22:38:06 +0300

    Creating new component

--- codvolution-redux/src/container/NewCakeContainer.js
+++ codvolution-redux/src/container/NewCakeContainer.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
-import { buyCake } from '../redux';
+import { buyCake } from '../redux'
 
-const NewCakeContainer = () => {
+function CakeContainer(props) {
     return (
         <div>
-            
+            <h1>Number of cakes: {props.numberOfCakes}</h1>
+            <button onClick= { props.buyCake }>Buy cakes</button>
         </div>
     )
 }
 
-export default NewCakeContainer
+const mapStateToProps = state => {
+    return {
+        numberOfCakes: state.cake.numberOfCakes,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
