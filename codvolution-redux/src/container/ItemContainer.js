cd64e47e8b1870295819cf2414862b105e42bab0
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-08 19:32:18 +0300

    Add mapdispatch to prop

--- codvolution-redux/src/container/ItemContainer.js
+++ codvolution-redux/src/container/ItemContainer.js
@@ -6,6 +6,7 @@ function ItemContainer(props){
     return (
         <div>
             <h2>Item - { props.item } </h2>
+            <button onClick= {props.buyItem }> Buy item </button>
         </div>
     )
 }
