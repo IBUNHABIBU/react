e172f3a724e755eab7bada1bb19c8852980712a3
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-20 19:22:04 +0300

    Add make Style

--- api_todos_react/src/TodoList/TodoItem.js
+++ api_todos_react/src/TodoList/TodoItem.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Grid, Paper } from '@material-ui/core'
+import React from 'react';
+import { Grid, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+const useStyle = makeStyles()
 
 const TodoItem = (props) => {
     return (
-        <Grid container spacing={3}>
-            <Paper>
-                {props.item.task}
-            </Paper>
+        <Grid container spacing={0}>
+            <Grid item xs={9}>
+                <Paper elevation={1}>
+                    {props.item.task}
+                </Paper>
+            </Grid>
         </Grid>
     )
 }
