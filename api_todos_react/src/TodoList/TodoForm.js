da847dbb0f745f2c0ad124323311b5b67e35e056
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-22 16:32:53 +0300

    Style the body

--- api_todos_react/src/TodoList/TodoForm.js
+++ api_todos_react/src/TodoList/TodoForm.js
@@ -7,8 +7,10 @@ export default class TodoForm extends Component {
         this.state = {
              api_url: props.api_url,
              task: "",
+             body: "",
         }
-        this.handleChange=this.handleChange.bind(this)
+        this.handleTaskChange=this.handleTaskChange.bind(this)
+        this.handleBodyChange=this.handleBodyChange.bind(this)
         this.handleSubmit=this.handleSubmit.bind(this)
     }
     
@@ -21,11 +23,19 @@ export default class TodoForm extends Component {
         }).then(response => response.json())
         .then(response => this.props.updateToDoList(response))
     }
-    handleChange(e){
+
+    handleTaskChange(e){
         this.setState({
             task: e.target.value
         })
     }
+
+    handleBodyChange(e){
+        this.setState({
+            body: e.target.value
+        })
+    }
+
     handleSubmit(e) {
         e.preventDefault();
         this.formSubmit(e.target)
@@ -46,7 +56,8 @@ export default class TodoForm extends Component {
                                 variant ="outlined"
                                 name="todo[task]"
                                 fullWidth
-                                onChange={this.handleChange} 
+                                value={this.state.task}
+                                onChange={this.handleTaskChange} 
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -57,7 +68,9 @@ export default class TodoForm extends Component {
                                     type="text"
                                     name="todo[body]"
                                     rowsMin={3}
-                                    placeholder="describe your todo"
+                                    placeholder="describe your todo"  
+                                    value={this.state.body}
+                                    onChange = { this.handleBodyChange }
                                     style={{ width: '100%'}}
                                 />
                             </Grid>
