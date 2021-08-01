6886fc736e3ada89036df1ae51529967fbfdc2be
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-01 20:01:39 +0300

    Add route to app.js

--- authReactApp/src/components/app.js
+++ authReactApp/src/components/app.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React + React Router</h2>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/Dashboard"} component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
