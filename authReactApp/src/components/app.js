a431fd6f30857ab67b81c5d0fd3e5d5d3d54ddb0
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-01 20:25:06 +0300

    Adding route

--- authReactApp/src/components/app.js
+++ authReactApp/src/components/app.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import Dashboard from './Dashboard';
 
 export default class App extends Component {
   render() {
