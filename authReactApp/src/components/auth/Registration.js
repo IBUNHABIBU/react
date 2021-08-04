b0b8f9b86ebf74b1f1dfb4799de74bb741b20a10
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-04 17:05:50 +0300

    Add registration components

--- authReactApp/src/components/auth/Registration.js
+++ authReactApp/src/components/auth/Registration.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Registration extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
     render() {
         return (
             <div>
-                
+                <h1>This is registration</h1>
             </div>
         )
     }
