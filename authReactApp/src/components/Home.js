09d8b0b4ab4e84d8beae8de2bd8527afdcbd6732
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-04 22:41:17 +0300

    render the home

--- authReactApp/src/components/Home.js
+++ authReactApp/src/components/Home.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Registration from './auth/Registration';
 
-const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
-            <Registration />
-        </div>
-    )
+export default class Home extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <h2>Status : { this.props.loggedInStatus }</h2>
+            </div>
+        )
+    }
 }
 
-export default Home
